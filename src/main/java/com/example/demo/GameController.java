5119f9846eba13776342529b6c84c12dded4b281
Martin Lindvall <martin_lindvall@hotmail.com>
2019-11-20 16:21:39 +0100

    testCommit

---
+++ src/main/java/com/example/demo/GameController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class GameController {
+
+    @GetMapping("/grid")
+    public String gameBoard(HttpSession session) {
+        GameLogic gameLogic = (GameLogic)session.getAttribute("gameLogicKey");
+        if(gameLogic == null) {
+            gameLogic = new GameLogic();
+            gameLogic.createCards(Main.numOfPlayingCards);
+            //gameLogic.shuffleCards();
+            gameLogic.splitListOfCards();
+            session.setAttribute("gameLogicKey", gameLogic);
+        }
+
+        return "gameGrid";
+    }
+
+}
--- src/main/java/com/example/demo/GameLogic.java
+++ src/main/java/com/example/demo/GameLogic.java
@@ -1,12 +1,41 @@
 package com.example.demo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GameLogic {
 
     private int numOfCards;
     private List<MemoryCard> listOfCards;
+    private List<MemoryCard> subList1;
+    private List<MemoryCard> subList2;
+    private List<MemoryCard> subList3;
+    private List<MemoryCard> subList4;
+
+    public List<MemoryCard> getSubList1() {
+        return subList1;
+    }
+
+    public List<MemoryCard> getSubList2() {
+        return subList2;
+    }
+
+    public List<MemoryCard> getSubList3() {
+        return subList3;
+    }
+
+    public List<MemoryCard> getSubList4() {
+        return subList4;
+    }
+
+    public List<MemoryCard> getListOfCards() {
+        return listOfCards;
+    }
+
+    public void setListOfCards(List<MemoryCard> listOfCards) {
+        this.listOfCards = listOfCards;
+    }
 
     public List<MemoryCard> createCards(int numOfCards){
 
@@ -14,9 +43,7 @@ public class GameLogic {
         int j = 1;
 
         for (int i = 0; i < (numOfCards); i++) {
-
             MemoryCard card;
-
             if(i< 8){
                 card = new MemoryCard(false, (i+1), "card"+(i+1)+".png");
             }
@@ -25,11 +52,20 @@ public class GameLogic {
                 j++;
             }
             listOfCards.add(card);
-
         }
 
         return listOfCards;
     }
 
+    public void splitListOfCards() {
+        subList1 = listOfCards.subList(0, 4);
+        subList2 = listOfCards.subList(4, 8);
+        subList3 = listOfCards.subList(8, 12);
+        subList4 = listOfCards.subList(12, 16);
+    }
+
+    public void shuffleCards() {
+        Collections.shuffle(listOfCards);
+    }
 
 }
