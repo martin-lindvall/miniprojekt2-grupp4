7c89b54e1a8e4ecfc4ace5265f86e46608c8f0b0
mdahlin <michaeladahlin4@gmail.com>
2019-12-05 09:52:48 +0100

    Added DB, connection and test

--- src/main/java/com/example/demo/userRepository.java
+++ src/main/java/com/example/demo/userRepository.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Repository
 public class userRepository {
+
+    @Autowired
+    DataSource dataSource;
+
     private List<UserInfo> users;
     private List<Integer> userLowScore = new ArrayList<>();
 
@@ -37,6 +49,20 @@ public class userRepository {
 
     public List<Integer> getUserLowScore() {
         return userLowScore;
+    }
+
+    public boolean testDB() throws SQLException {
+        int two = 0;
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT 1+1")) {
+            rs.next();
+            two = rs.getInt(1);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return two == 2;
     }
     //    public UserInfo getUserInfo (String userName, String password){
 //    for (UserInfo users : userInfo) {
--- src/test/java/com/example/demo/GameLogicTest.java
+++ src/test/java/com/example/demo/GameLogicTest.java
@@ -3,12 +3,17 @@ package com.example.demo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @SpringBootTest
 class GameLogicTest {
+
+	@Autowired
+	userRepository repository;
 
 	//static GameLogic sut;
 	static GameLogic sut;
@@ -26,6 +31,11 @@ class GameLogicTest {
 	public void numberOfPlayingCards(){
 		List<MemoryCard> listOfCards = sut.createCards(16);
 		Assertions.assertEquals(16, listOfCards.size());
+	}
+
+	@Test
+	void testSQLServler() throws SQLException {
+		Assertions.assertTrue(repository.testDB());
 	}
 
 //	@Test
