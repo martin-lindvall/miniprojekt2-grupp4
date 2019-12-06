156a60f0b276ebb56f0c2115bf32892f0ff1283d
mdahlin <michaeladahlin4@gmail.com>
2019-12-06 13:48:16 +0100

    comment code

--- src/main/java/com/example/demo/UserInfo.java
+++ src/main/java/com/example/demo/UserInfo.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 public class UserInfo {
 
